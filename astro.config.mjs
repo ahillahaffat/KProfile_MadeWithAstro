98abd097f8d0e6388d091c158150110de517b526
ahillahaffat <kammala88@gmail.com>
2026-08-09 00:46:45 +0700

    add config to get image assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from "@astrojs/sitemap";
-import preact from "@astrojs/preact";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  // Add your domain name here
-  site: "https://lexingtonthemes.com/",
-  integrations: [sitemap(), preact()]
+
+  site: 'https://lexingtonthemes.com/',
 });
---
+++ src/config/work-experience.ts
@@ -0,0 +1,21 @@
+export const workExperience = [
+    {
+        company: "Kementerian Keuangan Republik Indonesia",
+        src: "/kemenkeu.png",
+        alt: "Kemenkeu RI",
+        position: "Fullstack Developer",
+        startDate: "2024-02-17",
+        endDate: "2024-07-01",
+        description: "Worked on various projects and contributed to the development of new features.",
+    },
+    {
+        company: "Molca Teknologi Nusantara",
+        src: "/molca.png",
+        alt: "Molca Teknologi Nusantara",
+        position: "Fullstack Developer",
+        startDate: "2025-07-03",
+        endDate: "2025-10-01",
+        description: "Leading a team of engineers and working on cutting-edge technologies.",
+    },
+];
+
